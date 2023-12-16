5e7e32f608c7ba2bef2b443e22eb75b925a1727a
lostyoru <l_bayarassou@estin.dz>
2023-12-16 11:41:49 +0100

    fix

--- controllers/userController.js
+++ controllers/userController.js
@@ -4,7 +4,7 @@ const bcrypt = require('bcrypt');;
 async function getUsers(req, res) {
     try {
         const users = await User.find();
-        return res.status(200).json({ users });
+        return res.status(200).json(users);
     } catch (error) {
         return res.status(500).json({ message: 'Something went wrong' });
     }
